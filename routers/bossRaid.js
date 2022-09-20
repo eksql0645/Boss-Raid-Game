126ab01a28e831bc2119e24d2df2bcb79db8370e
JoKyungseo <eksql0645@gmail.com>
2022-09-20 21:15:47 +0900

    [Docs] swagger 작성

--- routers/bossRaid.js
+++ routers/bossRaid.js
@@ -62,7 +62,7 @@ bossRaidRouter.post("/", getEnterStatusValidator(), async (req, res, next) => {
       enterInfo
     );
 
-    res.status(201).json(enterStatus);
+    res.status(200).json(enterStatus);
   } catch (err) {
     next(err);
   }
@@ -81,5 +81,200 @@ bossRaidRouter.patch("/", endRaidValidator(), async (req, res, next) => {
     next(err);
   }
 });
+
+/**
+ * @swagger
+ * paths:
+ *   /api/bossraids/{userId}:
+ *    get:
+ *      summary:  "보스레이드 랭킹 조회"
+ *      description: "보스레이드 전체 및 유저 랭킹을 조회합니다."
+ *      tags: [BossRaid]
+ *      parameters :
+ *         - in : path
+ *           name : userId
+ *           required : true
+ *           description : 유저 id
+ *           schema :
+ *              type : String
+ *      responses:
+ *        "200":
+ *          description: "보스레이드 전체 및 유저 랭킹을 반환합니다."
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : object
+ *                  example:
+ *                        {
+ *                            "ranking": [
+ *                                {
+ *                                    "ranking": 1,
+ *                                    "userId": "NIq7FTQQ7cf_6X3N1Dti8",
+ *                                    "totalScore": 4100
+ *                                },
+ *                                {
+ *                                    "ranking": 2,
+ *                                    "userId": "S1f4CvnbpUr2-dLcifzcS",
+ *                                    "totalScore": 600
+ *                                },
+ *                                {
+ *                                    "ranking": 3,
+ *                                    "userId": "UPEj_StZ6ABw7dfCPtDqk",
+ *                                    "totalScore": 300
+ *                                },
+ *                                {
+ *                                    "ranking": 3,
+ *                                    "userId": "-NyZapMP_MDfJeVs-C_HJ",
+ *                                    "totalScore": 300
+ *                                },
+ *                            ],
+ *                            "userRanking": [
+ *                                {
+ *                                    "ranking": 1,
+ *                                    "userId": "NIq7FTQQ7cf_6X3N1Dti8",
+ *                                    "totalScore": 4100
+ *                                }
+ *                            ]
+ *                        }
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error: [
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                     ]
+ *                  }
+ *
+ */
+
+/**
+ * @swagger
+ * paths:
+ *   /api/bossraids:
+ *    get:
+ *      summary:  "보스레이드 상태 조회"
+ *      description: "보스레이드 상태를 조회합니다."
+ *      tags: [BossRaid]
+ *      responses:
+ *        "200":
+ *          description: "보스레이드 상태를 반환합니다."
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : object
+ *                  example:
+ *                    {
+ *                        "canEnter": true,
+ *                        "enteredUserId": null
+ *                    }
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error: [
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                     ]
+ *                  }
+ *
+ */
+
+/**
+ * @swagger
+ * paths:
+ *   /api/bossraids:
+ *    post:
+ *      summary:  "보스레이드 시작"
+ *      description: "보스레이드를 시작합니다."
+ *      tags: [BossRaid]
+ *      parameters :
+ *         - in : body
+ *           name : data
+ *           required : true
+ *           description : 생성할 데이터
+ *           schema :
+ *              type : object
+ *              example :
+ *                {userId: "wb7BO31yLajL4qT0QQAd5", level: 0}
+ *      responses:
+ *        "201":
+ *          description: "보스레이드 입장 상태를 반환합니다."
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : object
+ *                  example:
+ *                    {
+ *                        "isEntered": true,
+ *                        "raidRecordId": "wgbz0YgJQel6kZ-fUGv4i"
+ *                    }
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error: [
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                     ]
+ *                  }
+ *
+ */
+
+/**
+ * @swagger
+ * paths:
+ *   /api/bossraids:
+ *    patch:
+ *      summary:  "보스레이드 종료"
+ *      description: "보스레이드를 종료하고 정상 종료 시 보스레이드 기록을 생성하고 유저의 totalScore를 갱신합니다."
+ *      tags: [BossRaid]
+ *      parameters :
+ *         - in : body
+ *           name : data
+ *           required : true
+ *           description : 생성할 데이터
+ *           schema :
+ *              type : object
+ *              example :
+ *                {userId: "wb7BO31yLajL4qT0QQAd5", raidRecordId: "gWYKTGIKXlEFTtSKOfkX1"}
+ *      responses:
+ *        "200":
+ *          description: "상태코드만 반환합니다."
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error: [
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                     ]
+ *                  }
+ *
+ */
 
 module.exports = bossRaidRouter;
--- routers/user.js
+++ routers/user.js
@@ -33,4 +33,108 @@ userRouter.get("/:userId", userGetValidator(), async (req, res, next) => {
   }
 });
 
+/**
+ * @swagger
+ * paths:
+ *   /api/users:
+ *    post:
+ *      summary:  "유저 생성"
+ *      description: "유저를 생성합니다."
+ *      tags: [BossRaid]
+ *      parameters :
+ *         - in : body
+ *           name : data
+ *           required : true
+ *           description : 생성할 데이터
+ *           schema :
+ *              type : object
+ *              example :
+ *                {nick: "test", email: "tes00@stest.com", password: "1234"}
+ *      responses:
+ *        "201":
+ *          description: "생성된 userId를 반환합니다."
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : object
+ *                  example:
+ *                          {
+ *                               "userId": "ZkIDCwTiVwZZEQZ9oSqz8"
+ *                          }
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error: [
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                     ]
+ *                  }
+ *
+ */
+
+/**
+ * @swagger
+ * paths:
+ *   /api/users/{userId}:
+ *    get:
+ *      summary:  "유저 보스레이드 기록 조회"
+ *      description: "유저의 보스레이드 기록을 조회합니다."
+ *      tags: [BossRaid]
+ *      parameters :
+ *         - in : path
+ *           name : userId
+ *           required : true
+ *           description : 유저 id
+ *           schema :
+ *              type : String
+ *      responses:
+ *        "200":
+ *          description: "유저의 보스레이드 기록을 반환합니다."
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : object
+ *                  example:
+ *                        {
+ *                            "totalScore": 60,
+ *                            "BossRaidHistories": [
+ *                                {
+ *                                    "raidRecordId": "gWYKTGIKXlEFTtSKOfkX1",
+ *                                    "score": 20,
+ *                                    "enterTime": "2022-09-20T07:37:23.727Z",
+ *                                    "endTime": "2022-09-20T16:37:32+09:00"
+ *                                },
+ *                                {
+ *                                    "raidRecordId": "uHVYTVC21NCnBbe0REpOP",
+ *                                    "score": 20,
+ *                                    "enterTime": "2022-09-20T07:34:41.913Z",
+ *                                    "endTime": "2022-09-20T16:34:52+09:00"
+ *                                }
+ *                            ]
+ *                        }
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error: [
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                     ]
+ *                  }
+ *
+ */
+
 module.exports = userRouter;
