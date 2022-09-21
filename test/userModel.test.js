7bf4f8cfb4205c365629cd08a7f258d83424b77c
JoKyungseo <eksql0645@gmail.com>
2022-09-21 13:02:47 +0900

    [Test] userModel unitTest 작성

---
+++ test/userModel.test.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+jest.mock("../db/models/user");
+jest.mock("../db/models/bossRaidHistory");
+const User = require("../db/models/user");
+const BossRaidHistory = require("../db/models/bossRaidHistory");
+const testData = require("./userTestData");
+const {
+  createUser,
+  findUserByEmail,
+  findUserById,
+  findHistory,
+  incrementUser,
+} = require("../models/user");
+
+describe("createUser", () => {
+  const userInfo = testData.userInfo;
+  const user = testData.user;
+
+  test("user를 반환한다.", async () => {
+    User.create.mockReturnValue(user);
+    expect(await createUser(userInfo)).toEqual(user);
+  });
+});
+
+describe("findUserByEmail", () => {
+  const email = testData.email;
+  const user = testData.user;
+
+  test("user를 반환한다.", async () => {
+    User.findOne.mockReturnValue(user);
+    expect(await findUserByEmail(email)).toEqual(user);
+  });
+});
+
+describe("findUserById", () => {
+  const userId = testData.userId;
+  const user = testData.user;
+
+  test("user를 반환한다.", async () => {
+    User.findOne.mockReturnValue(user);
+    expect(await findUserById(userId)).toEqual(user);
+  });
+});
+
+describe("findHistory", () => {
+  const userId = testData.userId;
+  const history = testData.history;
+
+  test("유저의 보스레이드 기록을 반환한다.", async () => {
+    User.findOne.mockReturnValue(history);
+    expect(await findHistory(userId)).toEqual(history);
+  });
+});
+
+describe("incrementUser", () => {
+  const incrementInfo = testData.incrementInfo;
+
+  const result = [[undefined, 1]];
+  test("유저의 보스레이드 기록을 반환한다.", async () => {
+    User.increment.mockReturnValue(result);
+    expect(await incrementUser(incrementInfo)).toEqual(result);
+  });
+});
---
+++ test/userTestData.js
@@ -0,0 +1,30 @@
+module.exports = {
+  userId: "k5yHK7ckWyFMD0clH_re3",
+  email: "email1@email.test",
+  userInfo: {
+    nick: "nick",
+    email: "email1@email.test",
+    password: "123!32QWE@",
+  },
+  user: {
+    email: "email1@email.test",
+    id: "k5yHK7ckWyFMD0clH_re3",
+    nick: "nick",
+    password: "$2b$08$DFAsps9gyX0YiHseMMxf7eOM9BfLfLIlmDS8BIp2B.791UiazFEkW",
+    totalScore: 0,
+    createdAt: "2022-09-21T03:20:10.308Z",
+    updatedAt: "2022-09-21T03:20:10.308Z",
+  },
+  history: {
+    totalScore: 20,
+    BossRaidHistories: [
+      {
+        raidRecordId: "2Y_OooB7yMZGiwFCz09ac",
+        score: 20,
+        enterTime: "2022-09-21T03:49:39.720Z",
+        endTime: "2022-09-21T12:50:02+09:00",
+      },
+    ],
+  },
+  incrementInfo: { userId: "k5yHK7ckWyFMD0clH_re3", score: 20 },
+};
